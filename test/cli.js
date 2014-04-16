295bd3c8d92cd247e0c7990a4c03a717044461a7
Luke Karrys <lukekarrys@gmail.com>
2014-04-16 15:59:10 -0700

    dont send cli test output to /dev/null

--- test/cli.js
+++ test/cli.js
@@ -4,13 +4,13 @@ var sh = require('execSync');
 
 Lab.experiment('CLI', function () {
     Lab.test('Exits with 0', function (done) {
-        var r = sh.run('node ./index.js sample/config.js &> /dev/null');
+        var r = sh.run('node ./index.js sample/config.js');
         Lab.expect(r).to.equal(0);
         done();
     });
 
     Lab.test('Exits with non 0', function (done) {
-        var r = sh.run('node ./index.js sample/cong.js &> /dev/null');
+        var r = sh.run('node ./index.js sample/cong.js');
         Lab.expect(r).to.not.equal(0);
         done();
     });
